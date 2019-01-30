3f8e82709a0d8deeafc66edafcc3b34be23983ce
kjgamis <kjgamis@live.ca>
2019-01-30 14:16:34 -0500

    add sample configuration

--- src/lib/actions/sampleaction.js
+++ src/lib/actions/sampleaction.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function(settings) {
-  // TODO Perform some action.
+  // This is Library Loaded
+  var scriptUrl = "https://s.pinimg.com/ct/core.js";
+
+  if (!window.pintrk) {
+    window.pintrk = function () {
+      window.pintrk.queue.push(Array.prototype.slice.call(arguments));
+    };
+    var pintrk = window.pintrk;
+    pintrk.queue = [];
+    pintrk.version = "3.0";
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = scriptUrl;
+    var firstScriptInPage = document.getElementsByTagName("script")[0];
+    firstScriptInPage.parentNode.insertBefore(script, firstScriptInPage);
+  }
+  
+  pintrk("load", "YOUR_TAG_ID");
+  pintrk("page");
+
 };
--- src/lib/events/enterCode.js
+++ src/lib/events/enterCode.js
@@ -6,15 +6,19 @@ var enteredKeys = [];
 var password = '83,69,67,82,69,84'; // this spells the word 'secret'
 
 window.addEventListener('keydown', function (event) {
-  enteredKeys.push(event.keyCode)
-  enteredKeys.join(',')
-  console.log(enteredKeys)
+  enteredKeys.push(event.keyCode);
+  enteredKeys.join(',');
+  console.log(enteredKeys);
 
-  if (enteredKeys === password)
-})
+  if (enteredKeys === password) {
+    triggers.forEach(function(trigger){
+      trigger();
+    });
+  }
+});
 
 module.exports = function(settings, trigger) {
   // TODO Start watching for an event. Call trigger when the event occurs.
   
-
+  triggers.push(trigger);
 };

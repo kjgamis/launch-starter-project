7a54f5c9e152850baa5895eebbe29f75f1d59bfa
kjgamis <kjgamis@live.ca>
2019-01-30 14:16:48 -0500

    update dependencies

---
+++ .sandbox/container.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "dataElements": {},
+  "rules": [],
+  "extensions": {},
+  "property": {
+    "settings": {}
+  },
+  "buildInfo": {
+    "turbineVersion": "25.4.0",
+    "turbineBuildDate": "2019-01-25T15:20:46.419Z",
+    "buildDate": "2019-01-25T15:20:46.426Z",
+    "environment": "development"
+  }
+}

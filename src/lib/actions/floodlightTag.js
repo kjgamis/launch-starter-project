2e10034268fb02eca2fe57a7394026749cdf14c3
kjgamis <kjgamis@live.ca>
2019-01-31 17:04:38 -0500

    print to console floodlight tag details

--- src/lib/actions/floodlightTag.js
+++ src/lib/actions/floodlightTag.js
@@ -2,4 +2,5 @@
 
 module.exports = function(settings) {
   // TODO Perform some action.
+  console.log('Floodlight Tag - Advertiser ID: ' + settings.advertiserId + '. ' + 'Group ID: ' + settings.groupId + '. ' + 'Activity ID: ' + settings.activityId + '.');
 };

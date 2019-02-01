15f0760550caac6816aeb6436cca2ab59b819d4e
kjgamis <kjgamis@live.ca>
2019-02-01 09:45:44 -0500

    update

--- .sandbox/container.js
+++ .sandbox/container.js
@@ -1,30 +1,14 @@
 module.exports = {
   "dataElements": {},
-  "rules": [{
-    "id": "RL1548876090339",
-    "events": [{
-      "modulePath": "starter-project/src/lib/events/delay.js",
-      "settings": {
-        "delay": "2500"
-      }
-    }],
-    "actions": [{
-      "modulePath": "starter-project/src/lib/actions/alertMessage.js",
-      "settings": {
-        "visitorName": "Karen",
-        "message": "Welcome!"
-      }
-    }],
-    "name": "Karen's Welcome."
-  }],
+  "rules": [],
   "extensions": {},
   "property": {
     "settings": {}
   },
   "buildInfo": {
     "turbineVersion": "25.4.0",
-    "turbineBuildDate": "2019-01-30T19:21:32.682Z",
-    "buildDate": "2019-01-30T19:21:32.682Z",
+    "turbineBuildDate": "2019-02-01T14:44:36.388Z",
+    "buildDate": "2019-02-01T14:44:36.388Z",
     "environment": "development"
   }
 }

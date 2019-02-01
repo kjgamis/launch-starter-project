32c974f88a49e8f609f0b609be57049934516b17
kjgamis <kjgamis@live.ca>
2019-02-01 17:44:19 -0500

    password rule works

--- src/lib/events/enterCode.js
+++ src/lib/events/enterCode.js
@@ -7,15 +7,14 @@ var password = '83,69,67,82,69,84'; // this spells the word 'secret'
 
 window.addEventListener('keydown', function (event) {
   enteredKeys.push(event.keyCode);
-  enteredKeys.join(',');
   console.log(enteredKeys);
 
-  if (enteredKeys === password) {
+  if (enteredKeys.join(',') === password) {
     triggers.forEach(function(trigger){
       trigger();
     });
   }
-});
+}, true);
 
 module.exports = function(settings, trigger) {
   // TODO Start watching for an event. Call trigger when the event occurs.
